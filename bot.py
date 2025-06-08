c9658145d04b187973d5af8ebf3a6c2cb8da513d
kpidlubnyi <kpidlubnyi@gmail.com>
2025-06-08 16:08:28 +0200

    git init

---
+++ bot.py
@@ -0,0 +1,49 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler
+import requests
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+TOKEN = os.environ.get('BOT_TOKEN')
+
+async def start(update, context):
+    await update.mesage.reply_text('Siema!')
+
+async def help_command(update, context):
+    await update.message.reply_text("Nie będę ci pomagał")
+
+def get_currency(CUR):
+    if len(CUR) != 3 or not isinstance(CUR, str):
+        raise ValueError("Currency must be 3 symbols long!")
+    
+    CUR = CUR.strip().upper()
+    
+    try:
+        response = requests.get(rf'https://api.nbp.pl/api/exchangerates/rates/C/{CUR}/?format=json')
+        code, bid, ask = response.get('code'), response.get('rates')[0].get('bid'), response.get('rates')[0].get('ask')
+        return f'{code}: {bid} / {ask}'
+    except requests.exceptions.JSONDecodeError:
+        pass
+
+async def currency(update, context):
+    response = get_currency(context)
+    await update.message.reply_text(response)
+
+def main():
+    application = Application.builder().token(TOKEN).build()
+
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('help', help_command))
+    application.add_handler(CommandHandler('currency', currency))
+
+    PORT = int(os.environ.get('PORT', 8000))
+    application.run_webhook(
+        listen = '0.0.0.0',
+        port=PORT,
+        webhook_url=f''
+    )
+
+if __name__ == '__main__':
+    main()
